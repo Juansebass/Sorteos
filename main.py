69e9b3676a51f88290075e7c22a9ed3e2f56ab4e
Juansebass <juansebasacevedo@gmail.com>
2021-02-20 21:20:51 -0500

    Configurando SQLALchemy como ORM, Y creando base de datos inicial con SQLlite

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from flask import Flask, url_for, request, render_template, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+#Configuranfo base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
+db = SQLAlchemy(app)
+
+
+class Participantes(db.Model):
+    tipo_documento = db.Column(db.String(4))
+    numero_documento = db.Column(db.String(20), primary_key=True)
+    nombres = db.Column(db.String(100))
+    apellido =  db.Column(db.String(100))
+    fecha_nacimiento = db.Column(db.String(20))
+    recibio_premio = db.Column(db.String(2))
+    premio = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Participantes %r>' % self.nombres
+
+
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+def inicial():
     if request.method == 'POST':
         return 'Post'
     else:
